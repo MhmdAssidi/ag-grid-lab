a31dbc3a0a2be609d4e74b712f749608acc6bf44
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-15 14:59:48 +0300

    Initial commit - AG Grid base project with status chips and pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, ICellRendererParams } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+ModuleRegistry.registerModules([AllCommunityModule]);
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, AgGridAngular],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-grid-lab';
+
+  columnDefs: ColDef[] = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'name', headerName: 'Name' },
+    { field: 'description', headerName: 'Description' },
+    { field: 'price', headerName: 'Price' },
+    {
+  headerName: 'Status',
+  field: 'status',
+  cellRenderer: (params: ICellRendererParams) => {
+    const value = params.value as string;
+    const color = value === 'Available' ? '#4caf50' : '#f44336';
+
+    return `
+      <span style="
+        display: inline-block;
+        padding: 4px 10px;
+        background: ${color};
+        color: white;
+        font-weight: bold;
+        font-size: 16px;
+      ">
+        ${value}
+      </span>
+    `;
+  }
+}
+
+  ];
+
+  rowData = [
+    { id: 1, name: 'Product A', description: 'First item', price: 10, status: 'Available' },
+    { id: 2, name: 'Product B', description: 'Second item', price: 20, status: 'Out of Stock' },
+    { id: 3, name: 'Product C', description: 'Third item', price: 30, status: 'Available' },
+  ];
+  pageSize = 2;
+
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+    resizable: true,
+    flex: 1
+  };
+  
 }
